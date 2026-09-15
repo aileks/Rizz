ed8dd8fbd006e62c1c6e4b34af11507513ca1add
Kieran Pedrick <kieran@aileks.dev>
2026-09-15 16:35:15 -0400

    initial commit

---
+++ src/main/java/dev/aileks/Rizz/RizzApplication.java
@@ -0,0 +1,13 @@
+package dev.aileks.Rizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RizzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/aileks/Rizz/RizzApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aileks.Rizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
