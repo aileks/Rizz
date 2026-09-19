021c37b482e305c33392188400edafbbaac59ea0
Kieran Pedrick <kieran@aileks.dev>
2026-09-19 11:26:15 -0400

    update readme

--- src/main/java/dev/aileks/Rizz/RizzApplication.java
+++ src/main/java/dev/aileks/Rizz/RizzApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RizzApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RizzApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(RizzApplication.class, args);
+  }
 }
